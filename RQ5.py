e8db3153249bd6e035c4527d0e7f7a15c2c9a3af
PonjevicDino <75270990+PonjevicDino@users.noreply.github.com>
2025-05-20 00:44:01 +0200

    Finished RQ5

---
+++ RQ5.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import shutil
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pyarrow.compute as pc
+
+import numpy as np
+
+dataset_root = "./Dataset_Modified/2015"
+
+dataset = pq.ParquetDataset(dataset_root + "/")
+dataset_table = dataset.read().to_pandas()
+
+dataset_table['Trip_Distance'] = dataset_table['Trip_Distance'].replace(0, np.nan)
+dataset_table['Trip_Duration'] = dataset_table['Trip_Duration'].replace(0, np.nan)
+
+dataset_table['Tip_Per_Distance'] = dataset_table['Tip_Amount'] / dataset_table['Trip_Distance']
+dataset_table['Tip_Per_Duration'] = dataset_table['Tip_Amount'] / dataset_table['Trip_Duration']
+
+dataset_table = dataset_table.groupby('Type').agg(
+    Factor_Tip_Mile=('Tip_Per_Distance', 'mean'),
+    Factor_Tip_Minute=('Tip_Per_Duration', lambda x: x.mean() * 60)
+).reset_index()
+
+print(dataset_table)
+
+try:
+    dataset_table.to_parquet("./RQ5.parquet", index=False)
+except Exception as e:
+    print(e)
